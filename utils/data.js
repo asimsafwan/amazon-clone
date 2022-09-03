3d71acc2fa074e5869eb453aceac43020c413ed8
Asim Safwan <asim.safwan@gmail.com>
2022-09-03 15:27:46 +0530

    adding product images and product data such as price,etc

---
+++ utils/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      name: "Free Shirt",
+      slug: "free-shirt",
+      category: "Shirts",
+      image: "/images/shirt1.jpg",
+      price: 70,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: "A popular shirt",
+    },
+    {
+      name: "Slim Shirt",
+      slug: "slim-shirt",
+      category: "Shirts",
+      image: "/images/shirt3.jpg",
+      price: 90,
+      brand: "Raymond",
+      rating: 4.5,
+      numReviews: 3,
+      countInStock: 20,
+      description: "A popular shirt",
+    },
+    {
+      name: "Golf Pants",
+      slug: "golf-pants",
+      category: "Pants",
+      image: "/images/pants1.jpg",
+      price: 90,
+      brand: "Oliver",
+      rating: 2.9,
+      numReviews: 13,
+      countInStock: 20,
+      description: "Smart Looking pants",
+    },
+  ],
+};
+
+export default data;
